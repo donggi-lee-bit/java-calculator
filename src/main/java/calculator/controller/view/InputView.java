f1be13823437574608467b054464f4bd2caa7830
donggi-lee-bit <devdonggilee@gmail.com>
2024-02-16 01:53:14 +0900

    feat: 프로그램 종료 로직 추가

--- src/main/java/calculator/controller/view/InputView.java
+++ src/main/java/calculator/controller/view/InputView.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class InputView {
 
+    public static final String EXIT_COMMAND = "q";
+
     private Validator validator;
     private Scanner scanner;
 
@@ -15,12 +17,19 @@ public class InputView {
 
     public String[] readCalculationExpression() {
         System.out.println("\n 계산할 식을 입력해주세요 (e. g. 10 + 10). 종료하려면 'q'를 입력하세요: ");
-        String[] calculationExpression;
+        String input = scanner.nextLine().trim();
+
+        if (EXIT_COMMAND.equalsIgnoreCase(input)) {
+            System.out.println("프로그램을 종료합니다.");
+            System.exit(0);
+        }
+
+        final String[] calculationExpression = input.split(" ");
+
         try {
-            calculationExpression = scanner.nextLine().trim().split(" ");
             validator.validate(calculationExpression);
         } catch (IllegalArgumentException e) {
-            System.err.println("[ERROR] " + e.getMessage());
+            System.out.println("[ERROR] " + e.getMessage());
             return readCalculationExpression();
         }
         return calculationExpression;
