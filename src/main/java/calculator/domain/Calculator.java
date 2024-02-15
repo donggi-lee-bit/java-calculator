9ad2319888bd24db55794da1943f1f3556dde646
donggi-lee-bit <devdonggilee@gmail.com>
2024-02-16 00:44:56 +0900

    refactor: 중복 연산 결과 로직 메서드 추출

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -6,21 +6,23 @@ import java.math.RoundingMode;
 public class Calculator {
 
     public BigDecimal add(final BigDecimal num1, final BigDecimal num2) {
-        return num1.add(num2)
-            .setScale(1, RoundingMode.HALF_UP);
+        return operationResultFormat(num1.add(num2));
     }
 
     public BigDecimal subtract(final BigDecimal num1, final BigDecimal num2) {
-        return num1.subtract(num2)
-            .setScale(1, RoundingMode.HALF_UP);
+        return operationResultFormat(num1.subtract(num2));
+
     }
 
     public BigDecimal multiply(final BigDecimal num1, final BigDecimal num2) {
-        return num1.multiply(num2)
-            .setScale(1, RoundingMode.HALF_UP);
+        return operationResultFormat(num1.multiply(num2));
     }
 
     public BigDecimal divide(final BigDecimal num1, final BigDecimal num2) {
         return num1.divide(num2, 1, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal operationResultFormat(BigDecimal result) {
+        return result.setScale(1, RoundingMode.HALF_UP);
     }
 }
