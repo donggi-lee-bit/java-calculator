1605a51e75b693bccbe9eebb2416c551448bc142
Donggi Lee <devdonggilee@gmail.com>
2024-02-16 00:50:44 +0900

    Feature/operation (#2)
    * refactor: 실행 클래스 이름 변경
    
    * feat: 덧셈 연산 로직 추가
    
    * refactor: 소수점 연산을 허용하기 위해 BigDecimal 타입으로 변경
    
    * feat: 뺄셈 연산 로직 추가
    
    * feat: 곱셈 연산 로직 추가
    
    * test: 1보다 작은 소수점이 포함된 테스트 추가
    
    * feat: 나눗셈 연산 로직 추가
    
    * refactor: 중복 연산 결과 로직 메서드 추출

---
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -0,0 +1,124 @@
+package calculator.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNumbersForAddition")
+    void 덧셈이_성공하면_덧셈_결과를_반환한다(BigDecimal num1, BigDecimal num2, BigDecimal expected) {
+        BigDecimal actual = calculator.add(num1, num2);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNumbersForSubtraction")
+    void 뺄셈이_성공하면_뺄셈_결과를_반환한다(BigDecimal num1, BigDecimal num2, BigDecimal expected) {
+        BigDecimal actual = calculator.subtract(num1, num2);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNumbersForMultiplication")
+    void 곱셈이_성공하면_곱셈_결과를_반환한다(BigDecimal num1, BigDecimal num2, BigDecimal expected) {
+        BigDecimal actual = calculator.multiply(num1, num2);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNumbersForDivision")
+    void 나눗셈이_성공하면_나눗셈_결과를_반환한다(BigDecimal num1, BigDecimal num2, BigDecimal expected) {
+        BigDecimal actual = calculator.divide(num1, num2);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNumbersForDivisionException")
+    void 나눗셈이_0으로_나누는_경우_예외를_반환한다(BigDecimal num1, BigDecimal num2) {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(num1, num2));
+    }
+
+    private static Stream<Arguments> provideNumbersForAddition() {
+        return Stream.of(
+            Arguments.of(new BigDecimal("1.2"), new BigDecimal("2.3"), new BigDecimal("3.5")),
+            Arguments.of(new BigDecimal("2.3"), new BigDecimal("3.4"), new BigDecimal("5.7")),
+            Arguments.of(new BigDecimal("10.1"), new BigDecimal("2"), new BigDecimal("12.1")),
+            Arguments.of(new BigDecimal("1"), new BigDecimal("2"), new BigDecimal("3.0")),
+            Arguments.of(new BigDecimal("2"), new BigDecimal("3"), new BigDecimal("5.0")),
+            Arguments.of(new BigDecimal("10"), new BigDecimal("20"), new BigDecimal("30.0")),
+            Arguments.of(new BigDecimal("0.1"), new BigDecimal("0.2"), new BigDecimal("0.3")),
+            Arguments.of(new BigDecimal("0.5"), new BigDecimal("0.4"), new BigDecimal("0.9"))
+        );
+    }
+
+    private static Stream<Arguments> provideNumbersForSubtraction() {
+        return Stream.of(
+            Arguments.of(new BigDecimal("3.5"), new BigDecimal("2.3"), new BigDecimal("1.2")),
+            Arguments.of(new BigDecimal("5.7"), new BigDecimal("3.4"), new BigDecimal("2.3")),
+            Arguments.of(new BigDecimal("12.1"), new BigDecimal("2.0"), new BigDecimal("10.1")),
+            Arguments.of(new BigDecimal("3.0"), new BigDecimal("2.0"), new BigDecimal("1.0")),
+            Arguments.of(new BigDecimal("5.0"), new BigDecimal("3.0"), new BigDecimal("2.0")),
+            Arguments.of(new BigDecimal("30.0"), new BigDecimal("20.0"), new BigDecimal("10.0")),
+            Arguments.of(new BigDecimal("0.2"), new BigDecimal("0.1"), new BigDecimal("0.1")),
+            Arguments.of(new BigDecimal("0.5"), new BigDecimal("0.4"), new BigDecimal("0.1"))
+        );
+    }
+
+    private static Stream<Arguments> provideNumbersForMultiplication() {
+        return Stream.of(
+            Arguments.of(new BigDecimal("1.2"), new BigDecimal("2.3"), new BigDecimal("2.8")),
+            Arguments.of(new BigDecimal("2.3"), new BigDecimal("3.4"), new BigDecimal("7.8")),
+            Arguments.of(new BigDecimal("10.1"), new BigDecimal("2.0"), new BigDecimal("20.2")),
+            Arguments.of(new BigDecimal("1.0"), new BigDecimal("2.0"), new BigDecimal("2.0")),
+            Arguments.of(new BigDecimal("2.0"), new BigDecimal("3.0"), new BigDecimal("6.0")),
+            Arguments.of(new BigDecimal("10.0"), new BigDecimal("20.0"), new BigDecimal("200.0")),
+            Arguments.of(new BigDecimal("0.2"), new BigDecimal("0.1"), new BigDecimal("0.0")),
+            Arguments.of(new BigDecimal("0.5"), new BigDecimal("0.4"), new BigDecimal("0.2"))
+        );
+    }
+
+    private static Stream<Arguments> provideNumbersForDivision() {
+        return Stream.of(
+            Arguments.of(new BigDecimal("3.5"), new BigDecimal("2.3"), new BigDecimal("1.5")),
+            Arguments.of(new BigDecimal("5.7"), new BigDecimal("3.4"), new BigDecimal("1.7")),
+            Arguments.of(new BigDecimal("12.1"), new BigDecimal("2.0"), new BigDecimal("6.1")),
+            Arguments.of(new BigDecimal("3.0"), new BigDecimal("2.0"), new BigDecimal("1.5")),
+            Arguments.of(new BigDecimal("5.0"), new BigDecimal("3.0"), new BigDecimal("1.7")),
+            Arguments.of(new BigDecimal("30.0"), new BigDecimal("20.0"), new BigDecimal("1.5"))
+        );
+    }
+
+    private static Stream<Arguments> provideNumbersForDivisionException() {
+        return Stream.of(
+            Arguments.of(new BigDecimal("3.5"), BigDecimal.ZERO),
+            Arguments.of(new BigDecimal("5.7"), BigDecimal.ZERO),
+            Arguments.of(new BigDecimal("12.1"), BigDecimal.ZERO),
+            Arguments.of(new BigDecimal("3.0"), BigDecimal.ZERO),
+            Arguments.of(new BigDecimal("5.0"), BigDecimal.ZERO),
+            Arguments.of(new BigDecimal("30.0"), BigDecimal.ZERO)
+        );
+    }
+}
