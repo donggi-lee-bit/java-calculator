54a510e72ad2e1a77f178dea24650241c4ad4634
donggi-lee-bit <devdonggilee@gmail.com>
2024-02-16 00:09:06 +0900

    refactor: 소수점 연산을 허용하기 위해 BigDecimal 타입으로 변경

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,8 +1,11 @@
 package calculator.domain;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Calculator {
 
-    public int add(final int num1, final int num2) {
-        return num1 + num2;
+    public BigDecimal add(final BigDecimal num1, final BigDecimal num2) {
+        return num1.add(num2).setScale(1, RoundingMode.HALF_UP);
     }
 }
--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -2,22 +2,36 @@ package calculator.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigDecimal;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 public class CalculatorTest {
 
     @ParameterizedTest
-    @CsvSource({"1,2,3", "2,3,5", "10,20,30"})
-    void 덧셈이_성공하면_덧셈_결과를_반환한다(int num1, int num2, int expected) {
+    @MethodSource("provideNumbersForAddition")
+    void 덧셈이_성공하면_덧셈_결과를_반환한다(BigDecimal num1, BigDecimal num2, BigDecimal expected) {
         Calculator calculator = new Calculator();
 
-        int actual = calculator.add(num1, num2);
+        BigDecimal actual = calculator.add(num1, num2);
 
         assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> provideNumbersForAddition() {
+        return Stream.of(
+            Arguments.of(new BigDecimal("1.2"), new BigDecimal("2.3"), new BigDecimal("3.5")),
+            Arguments.of(new BigDecimal("2.3"), new BigDecimal("3.4"), new BigDecimal("5.7")),
+            Arguments.of(new BigDecimal("10.1"), new BigDecimal("2"), new BigDecimal("12.1")),
+            Arguments.of(new BigDecimal("1"), new BigDecimal("2"), new BigDecimal("3.0")),
+            Arguments.of(new BigDecimal("2"), new BigDecimal("3"), new BigDecimal("5.0")),
+            Arguments.of(new BigDecimal("10"), new BigDecimal("20"), new BigDecimal("30.0"))
+        );
     }
 }
