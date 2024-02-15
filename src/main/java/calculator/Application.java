0eab594e4ca80c9ffbf5d2cc3af9413d2179f32f
donggi-lee-bit <devdonggilee@gmail.com>
2024-02-16 03:04:10 +0900

    refactor: CalculatorController를 추가하여 입출력 로직을 CalculatorController 에서 처리하도록 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,32 +1,17 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
 import calculator.controller.validation.CalculationValidator;
 import calculator.controller.view.InputView;
 import calculator.controller.view.OutputView;
-import calculator.domain.Operator;
-import java.math.BigDecimal;
 
 public class Application {
 
     public static void main(String[] args){
-        InputView inputView = new InputView(new CalculationValidator());
-        OutputView outputView = new OutputView();
+        final InputView inputView = new InputView(new CalculationValidator());
+        final OutputView outputView = new OutputView();
 
-        while (true) {
-            try {
-                String[] expression = inputView.readCalculationExpression();
-
-                BigDecimal operand1 = new BigDecimal(expression[0]);
-                Operator operator = Operator.fromSymbol(expression[1]);
-                BigDecimal operand2 = new BigDecimal(expression[2]);
-
-                BigDecimal result = operator.apply(operand1, operand2);
-
-                outputView.displayResult(result);
-            } catch (ArithmeticException e) {
-                System.out.println("[ERROR] " + e.getMessage());
-            }
-
-        }
+        final CalculatorController calculatorController = new CalculatorController(inputView, outputView);
+        calculatorController.execute();
     }
 }
