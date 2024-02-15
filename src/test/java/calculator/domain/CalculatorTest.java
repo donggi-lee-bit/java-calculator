0c473333a136ea12c4563bfcfe8394a6cd55483a
donggi-lee-bit <devdonggilee@gmail.com>
2024-02-16 00:24:24 +0900

    test: 1보다 작은 소수점이 포함된 테스트 추가

--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -53,7 +53,9 @@ public class CalculatorTest {
             Arguments.of(new BigDecimal("10.1"), new BigDecimal("2"), new BigDecimal("12.1")),
             Arguments.of(new BigDecimal("1"), new BigDecimal("2"), new BigDecimal("3.0")),
             Arguments.of(new BigDecimal("2"), new BigDecimal("3"), new BigDecimal("5.0")),
-            Arguments.of(new BigDecimal("10"), new BigDecimal("20"), new BigDecimal("30.0"))
+            Arguments.of(new BigDecimal("10"), new BigDecimal("20"), new BigDecimal("30.0")),
+            Arguments.of(new BigDecimal("0.1"), new BigDecimal("0.2"), new BigDecimal("0.3")),
+            Arguments.of(new BigDecimal("0.5"), new BigDecimal("0.4"), new BigDecimal("0.9"))
         );
     }
 
@@ -64,7 +66,9 @@ public class CalculatorTest {
             Arguments.of(new BigDecimal("12.1"), new BigDecimal("2.0"), new BigDecimal("10.1")),
             Arguments.of(new BigDecimal("3.0"), new BigDecimal("2.0"), new BigDecimal("1.0")),
             Arguments.of(new BigDecimal("5.0"), new BigDecimal("3.0"), new BigDecimal("2.0")),
-            Arguments.of(new BigDecimal("30.0"), new BigDecimal("20.0"), new BigDecimal("10.0"))
+            Arguments.of(new BigDecimal("30.0"), new BigDecimal("20.0"), new BigDecimal("10.0")),
+            Arguments.of(new BigDecimal("0.2"), new BigDecimal("0.1"), new BigDecimal("0.1")),
+            Arguments.of(new BigDecimal("0.5"), new BigDecimal("0.4"), new BigDecimal("0.1"))
         );
     }
 
@@ -75,7 +79,9 @@ public class CalculatorTest {
             Arguments.of(new BigDecimal("10.1"), new BigDecimal("2.0"), new BigDecimal("20.2")),
             Arguments.of(new BigDecimal("1.0"), new BigDecimal("2.0"), new BigDecimal("2.0")),
             Arguments.of(new BigDecimal("2.0"), new BigDecimal("3.0"), new BigDecimal("6.0")),
-            Arguments.of(new BigDecimal("10.0"), new BigDecimal("20.0"), new BigDecimal("200.0"))
+            Arguments.of(new BigDecimal("10.0"), new BigDecimal("20.0"), new BigDecimal("200.0")),
+            Arguments.of(new BigDecimal("0.2"), new BigDecimal("0.1"), new BigDecimal("0.0")),
+            Arguments.of(new BigDecimal("0.5"), new BigDecimal("0.4"), new BigDecimal("0.2"))
         );
     }
 }
